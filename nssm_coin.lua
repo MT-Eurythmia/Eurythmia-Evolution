80fa5c83d209ea15fda0e76c8cf72542a4aacdc6
Tai @ Flex <dch.tai@gmail.com>
2016-08-14 12:31:27 +0100

    prelminary code for the NSSM coins

---
+++ nssm_coin.lua
@@ -0,0 +1,9 @@
+if minetest.get_modpath("nssm") then
+	--[[
+
+	food coins -- made from foods
+	night coins -- made from night monsters
+	ice coins -- made from ice monsters
+	might need to define groups...?
+	-- ]]
+end
