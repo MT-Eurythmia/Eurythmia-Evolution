56048ad495b138d53d11499e0fe49831accbcf58
Tai @ Flex <dch.tai@gmail.com>
2016-08-14 12:19:56 +0100

    Cleanup for multiple features

--- init.lua
+++ init.lua
@@ -1,21 +1,2 @@
--- Falling Light
--- A simple mod for explorers
--- (C) Tai "DuCake" Kedzierski 2016
--- Provided to you under 3-Clause BSD
-
-minetest.register_node("falling_light:light", {
-	description = "Falling Light",
-	paramtype = "light",
-	light_source = 14,
-	light_propagates = true,
-	sunlight_propagates = true,
-	tiles = {"default_sand.png"},
-	groups = {crumbly = 3, falling_node = 1},
-})
-
-minetest.register_craft ({
-        output = "falling_light:light 2",
-	type = "shapeless",
-        recipe = {"group:sand","default:torch"}
-}
-)
+-- Load the Falling Light mod
+dofile(minetest.get_modpath("vivarium") .. "/falling_light.lua")
