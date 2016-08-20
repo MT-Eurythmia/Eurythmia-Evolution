2c877dfc9667eaf9fa84c22dc54232141283cd52
Tai @ Flex <dch.tai@gmail.com>
2016-08-20 13:52:09 +0100

    some examples

--- bestiary.lua
+++ bestiary.lua
@@ -1,20 +1,18 @@
 -- Supported animals
 
-local bestiary = {
+vivarium.bestiary = {
 	{
 		name = "dmobs",
 		beasts = {"panda","badger"}, -- leave their types alone
 		animals = {"elephant","fox"}, -- make sure these are "animal"
 		monsters = {"ogre","orc"}, -- make sure these are "monster"
-		npcs = {"medved","dragon"}, -- make sure these are NPC.
+		npcs = {"dragon"}, -- make sure these are NPC.
 		follow = "farming:bread"
 	},
 	{
 		name = "nssm",
 		animals = nil,
 		monsters = {"masticone","phoenix"},
-		npcs = {"mese_companion"}
 	},
 }
 
-bestiary = {} -- turn it off for now
