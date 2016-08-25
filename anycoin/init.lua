dba946640ae2c45a3be92089198a466fbb21ec4a
Tai @ Flex <dch.tai@gmail.com>
2016-08-25 19:25:12 +0100

    standardized coins

--- anycoin/init.lua
+++ anycoin/init.lua
@@ -6,31 +6,29 @@ if minetest.get_modpath("moreores") then
 	metals = "moreores:tin_ingot"
 end
 
-function anycoin:register_coin(coinname,coindesc,cmaterial,cvalue)
-	local overimg = cmaterial.inventory_image or "default_dirt_with_grass.png"
+function anycoin:register_coin(coinname,coindesc,cmaterial,overimg,cvalue)
+
 	local coinimg = "anycoin_coin.png^"..overimg
 	local value = cvalue
 	if not cvalue then
 		cvalue = 1
 	end
 
-	minetest.register_craft_item("anycoin:coin_"..coinname, {
+	minetest.register_craftitem(":anycoin:coin_"..coinname, {
 		description = coindesc .. " ("..cvalue.." ac-)",
-		inventory_image = coingimg,
+		inventory_image = coinimg,
 		coinvalue = cvalue
 	})
 	
-	if cmaterial =~ nil then
-		core.register_craft({
-			type = "shapeless",
-			output = "anycoin:coin_base "..cvalue,
-			recipe = {"anycoin:coin_"..coinname}
-		}
-		)
-	end
-
+	core.register_craft({
+		type = "shapeless",
+		output = "anycoin:coin_base "..cvalue,
+		recipe = {"anycoin:coin_"..coinname}
+	}
+	)
+	
+	if cmaterial == "moreores:tin_ingot" then metals = "default:coal_lump" end
 	core.register_craft( {
-
 		output = "anycoin:coin_"..coinname,
 		recipe = {
 			{metals,cmaterial,metals},
@@ -43,7 +41,18 @@ function anycoin:register_coin(coinname,coindesc,cmaterial,cvalue)
 end
 
 -- A base coin to serve as an exchange base
-anycoin:register_coin("base","AnyCoin",nil,1)
+
+minetest.register_craftitem("anycoin:coin_base", {
+	description = "AnyCoin (1 ac-)",
+	inventory_image = "anycoin_coin.png",
+	coinvalue = 1,
+})
+
+minetest.register_craftitem("anycoin:coin_base9", {
+	description = "NineyCoin (9 ac-)",
+	inventory_image = "anycoin_coin.png^[colorize:blue:60",
+	coinvalue = 9
+})
 
 local basecoin = "anycoin:coin_base"
 
@@ -58,9 +67,9 @@ core.register_craft( {
 
 })
 
-minetest.register_craft_item("anycoin:coin_base", {
-	description = "AnyCoin9 (9 ac-)",
-	inventory_image = "default_dirt_with_dry_grass.png",
-	coinvalue = 9
-})
-
+anycoin:register_coin("iron","Iron Coin","default:steel_ingot","[colorize:red:50",10)
+anycoin:register_coin("copper","Copper Coin","default:copper_ingot","[colorize:orange:50",15)
+anycoin:register_coin("bronze","Bronze Coin","default:bronze_ingot","[colorize:orange:90",20)
+anycoin:register_coin("gold","Gold Coin","default:gold_ingot","[colorize:yellow:90",30)
+anycoin:register_coin("diamond","Diamond Coin","default:diamond","default_diamond.png",40)
+anycoin:register_coin("mese","Mese Coin","default:mese_crystal","default_mese_crystal.png",50)
