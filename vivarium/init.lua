abdfdd128f82182c0ee91471bd9a28c31057eaeb
Tai @ Flex <dch.tai@gmail.com>
2016-08-25 19:27:46 +0100

    backwards compat

--- vivarium/init.lua
+++ vivarium/init.lua
@@ -11,12 +11,13 @@ vivarium:realias("petting","mobtamer")
 vivarium:realias("staffmagic","staff_clone")
 vivarium:realias("staffmagic","staff_stack")
 vivarium:realias("staffmagic","staff_boom")
-vivarium:realias("staffmagic","staff_creator")
+vivarium:realias("staffmagic","staff_creative")
 vivarium:realias("staffmagic","staff_melt")
-vivarium:realias("fallinglight","light")
+vivarium:realias("fallinglight","falling_light")
+--fallinglight:falling_light
 
 
 if minetest.get_modpath("nssm") then
-	anycoin:register_coin("scrausics","Scrausics Coin","nssm:raw_scrausics_wing",3)
-	anycoin:register_coin("moonheron","Moonheron Coin","nssm:raw_moonheron_leg",3)
+	anycoin:register_coin("scrausics","Scrausics Coin","nssm:raw_scrausics_wing","raw_scrausics_wing.png",3)
+	anycoin:register_coin("moonheron","Moonheron Coin","nssm:raw_moonheron_leg","rraw_scrausics_wing.png",3)
 end
