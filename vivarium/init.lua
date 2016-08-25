c0dd49e9b88c8a664c65a04e29cd8f5fc17b7c65
Tai @ Flex <dch.tai@gmail.com>
2016-08-25 21:03:20 +0100

    corect coin def for heron

--- vivarium/init.lua
+++ vivarium/init.lua
@@ -19,5 +19,5 @@ vivarium:realias("fallinglight","falling_light")
 
 if minetest.get_modpath("nssm") then
 	anycoin:register_coin("scrausics","Scrausics Coin","nssm:raw_scrausics_wing","raw_scrausics_wing.png",3)
-	anycoin:register_coin("moonheron","Moonheron Coin","nssm:raw_moonheron_leg","rraw_scrausics_wing.png",3)
+	anycoin:register_coin("moonheron","Moonheron Coin","nssm:heron_leg","heron_leg.png",3)
 end
