9eb03029b0d8d99c304ea6c8177b4beb6260bc8d
Tai @ Flex <dch.tai@gmail.com>
2017-01-24 09:16:23 +0000

    remove tincraft

--- tincraft/README.md
+++
@@ -1,5 +0,0 @@
-# Tincraft
-
-Tin, from moreores, is pretty useless in most games.
-
-This will be a place to put ideas of things to use tin for.
--- tincraft/inbox.lua
+++
@@ -1,9 +0,0 @@
-core.register_craft({ 
-	output = "inbox:empty", 
-	recipe = { 
-		{"","tincraft:strong_tin",""}, 
-		{"tincraft:strong_tin","","tincraft:strong_tin"}, 
-		{"tincraft:strong_tin","tincraft:strong_tin","tincraft:strong_tin"}, 
-	} 
-}) 
-
