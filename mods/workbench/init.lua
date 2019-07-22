b18b9a57e7c74a0080c91e103cd886a15c258214
upsilon <upsilon@langg.net>
2019-07-22 19:50:23 +0200

    Do not prevent the workbench from cutting wool

--- mods/workbench/init.lua
+++ mods/workbench/init.lua
@@ -16,7 +16,6 @@ for node, def in pairs(minetest.registered_nodes) do
 	   not def.allow_metadata_inventory_take and
 	   not (def.groups.not_in_creative_inventory == 1) and
 	   not (def.groups.not_cuttable == 1) and
-	   not def.groups.wool and
 	   (def.tiles and type(def.tiles[1]) == "string" and not
 		def.tiles[1]:find("default_mineral")) and
 	   not def.mesecons and
