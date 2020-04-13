47f9df7886bc1961a27ddd087bed89f3dac5d0b2
upsilon <upsilon@langg.net>
2020-04-13 12:18:15 +0200

    git subrepo pull mods/throwing
    subrepo:
      subdir:   "mods/throwing"
      merged:   "49f3f36bd"
    upstream:
      origin:   "https://github.com/minetest-mods/throwing.git"
      branch:   "master"
      commit:   "49f3f36bd"
    git-subrepo:
      version:  "0.4.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a04d8c2"

--- mods/throwing/init.lua
+++ mods/throwing/init.lua
@@ -7,6 +7,7 @@ throwing.target_node = 2
 throwing.target_both = 3
 
 throwing.modname = minetest.get_current_modname()
+local use_toolranks = minetest.get_modpath("toolranks") and minetest.settings:get_bool("throwing.toolranks", true)
 
 --------- Arrows functions ---------
 function throwing.is_arrow(itemstack)
@@ -27,42 +28,81 @@ function throwing.spawn_arrow_entity(pos, arrow, player)
 	end
 end
 
-local function shoot_arrow(itemstack, player, index, throw_itself, new_stack)
+local function apply_realistic_acceleration(obj, mass)
+	if not minetest.settings:get_bool("throwing.realistic_trajectory", false) then
+		return
+	end
+
+	local vertical_acceleration = tonumber(minetest.settings:get("throwing.vertical_acceleration")) or -10
+	local friction_coef = tonumber(minetest.settings:get("throwing.frictional_coefficient")) or -3
+
+	local velocity = obj:get_velocity()
+	obj:set_acceleration({
+		x = friction_coef * velocity.x / mass,
+		y = friction_coef * velocity.y / mass + vertical_acceleration,
+		z = friction_coef * velocity.z / mass
+	})
+end
+
+local function shoot_arrow(def, toolranks_data, player, bow_index, throw_itself, new_stack)
 	local inventory = player:get_inventory()
-	if not throw_itself then
-		if index >= player:get_inventory():get_size("main") then
+	local arrow_index
+	if throw_itself then
+		arrow_index = bow_index
+	else
+		if bow_index >= player:get_inventory():get_size("main") then
 			return false
 		end
-		index = index + 1
+		arrow_index = bow_index + 1
 	end
-	local arrow_stack = inventory:get_stack("main", index)
+	local arrow_stack = inventory:get_stack("main", arrow_index)
 	local arrow = arrow_stack:get_name()
 
 	local playerpos = player:get_pos()
 	local pos = {x=playerpos.x,y=playerpos.y+1.5,z=playerpos.z}
-	local obj = (minetest.registered_items[itemstack:get_name()].spawn_arrow_entity or throwing.spawn_arrow_entity)(pos, arrow, player)
+	local obj = (def.spawn_arrow_entity or throwing.spawn_arrow_entity)(pos, arrow, player)
 
 	local luaentity = obj:get_luaentity()
+
+	-- Set custom data in the entity
 	luaentity.player = player:get_player_name()
 	if not luaentity.item then
 		luaentity.item = arrow
 	end
+	luaentity.data = {}
+	luaentity.timer = 0
+	luaentity.toolranks = toolranks_data -- May be nil if toolranks is disabled
 
 	if luaentity.on_throw then
-		if luaentity:on_throw(pos, player, arrow_stack, index, luaentity.data) == false then
+		if luaentity:on_throw(pos, player, arrow_stack, arrow_index, luaentity.data) == false then
 			obj:remove()
 			return false
 		end
 	end
 
 	local dir = player:get_look_dir()
-	local velocity_factor = tonumber(minetest.settings:get("throwing.velocity_factor")) or 19
-	local horizontal_acceleration_factor = tonumber(minetest.settings:get("throwing.horizontal_acceleration_factor")) or -3
 	local vertical_acceleration = tonumber(minetest.settings:get("throwing.vertical_acceleration")) or -10
+	local velocity_factor = tonumber(minetest.settings:get("throwing.velocity_factor")) or 19
+	local velocity_mode = minetest.settings:get("throwing.velocity_mode") or "strength"
 
-	obj:set_velocity({x=dir.x*velocity_factor, y=dir.y*velocity_factor, z=dir.z*velocity_factor})
-	obj:set_acceleration({x=dir.x*horizontal_acceleration_factor, y=vertical_acceleration, z=dir.z*horizontal_acceleration_factor})
+	local velocity
+	if velocity_mode == "simple" then
+		velocity = velocity_factor
+	elseif velocity_mode == "momentum" then
+		velocity = def.strength * velocity_factor / luaentity.mass
+	else
+		velocity = def.strength * velocity_factor
+	end
+
+	obj:set_velocity({
+		x = dir.x * velocity,
+		y = dir.y * velocity,
+		z = dir.z * velocity
+	})
+	obj:set_acceleration({x = 0, y = vertical_acceleration, z = 0})
 	obj:set_yaw(player:get_look_horizontal()-math.pi/2)
+
+	apply_realistic_acceleration(obj, luaentity.mass)
 
 	if luaentity.on_throw_sound ~= "" then
 		minetest.sound_play(luaentity.on_throw_sound or "throwing_sound", {pos=playerpos, gain = 0.5})
@@ -70,18 +110,23 @@ local function shoot_arrow(itemstack, player, index, throw_itself, new_stack)
 
 	if not minetest.settings:get_bool("creative_mode") then
 		if new_stack then
-			inventory:set_stack("main", index, new_stack)
+			inventory:set_stack("main", arrrow_index, new_stack)
 		else
-			local stack = inventory:get_stack("main", index)
+			local stack = inventory:get_stack("main", arrow_index)
 			stack:take_item()
-			inventory:set_stack("main", index, stack)
+			inventory:set_stack("main", arrow_index, stack)
 		end
 	end
 
 	return true
 end
 
-local function arrow_step(self, dtime)
+function throwing.arrow_step(self, dtime)
+	if not self.timer or not self.player then
+		self.object:remove()
+		return
+	end
+
 	self.timer = self.timer + dtime
 	local pos = self.object:get_pos()
 	local node = minetest.get_node(pos)
@@ -153,6 +198,16 @@ local function arrow_step(self, dtime)
 				logging("collided with object at ("..pos.x..","..pos.y..","..pos.z..")")
 			end
 		end
+
+		-- Toolranks support: update bow uses
+		if self.toolranks then
+			local inventory = player:get_inventory()
+			-- Check that the player did not move the bow
+			if inventory:get_stack("main", self.toolranks.index):get_name() == self.toolranks.itemstack:get_name() then
+				local new_itemstack = toolranks.new_afteruse(self.toolranks.itemstack, player, nil, {wear = self.toolranks.wear})
+				inventory:set_stack("main", self.toolranks.index, new_itemstack)
+			end
+		end
 	end
 
 	-- Collision with a node
@@ -200,14 +255,14 @@ local function arrow_step(self, dtime)
 		wielded_light.update_light_by_item(self.item, self.object:get_pos())
 	end
 
+	apply_realistic_acceleration(self.object, self.mass) -- Physics: air friction
+
 	self.last_pos = pos -- Used by the build arrow
 end
 
+-- Backwards compatibility
 function throwing.make_arrow_def(def)
-	def.timer = 0
-	def.player = ""
-	def.on_step = arrow_step
-	def.data = {}
+	def.on_step = throwing.arrow_step
 	return def
 end
 
@@ -234,6 +289,7 @@ function throwing.register_arrow(name, def)
 	if not def.groups.dig_immediate then
 		def.groups.dig_immediate = 3
 	end
+
 	def.inventory_image = def.tiles[1]
 	def.on_place = function(itemstack, placer, pointed_thing)
 		if minetest.settings:get_bool("throwing.allow_arrow_placing") and pointed_thing.above then
@@ -276,7 +332,7 @@ function throwing.register_arrow(name, def)
 	}
 	minetest.register_node(registration_name, def)
 
-	minetest.register_entity(registration_name.."_entity", throwing.make_arrow_def{
+	minetest.register_entity(registration_name.."_entity", {
 		physical = false,
 		visual = "wielditem",
 		visual_size = {x = 0.125, y = 0.125},
@@ -289,39 +345,36 @@ function throwing.register_arrow(name, def)
 		allow_protected = def.allow_protected,
 		target = def.target,
 		on_hit_fails = def.on_hit_fails,
+		on_step = throwing.arrow_step,
 		item = name,
+		mass = def.mass or 1,
 	})
-
-	if def.itemcraft then
-		minetest.register_craft({
-			output = name.." "..tostring(def.craft_quantity or 1),
-			recipe = {
-				{def.itemcraft, "default:stick", "default:stick"}
-			}
-		})
-		minetest.register_craft({
-			output = name.." "..tostring(def.craft_quantity or 1),
-			recipe = {
-				{ "default:stick", "default:stick", def.itemcraft}
-			}
-		})
-	end
 end
 
 
 ---------- Bows -----------
 function throwing.register_bow(name, def)
+	local enable_toolranks = use_toolranks and not def.no_toolranks
+
+	def.name = name
+
 	if not def.allow_shot then
 		def.allow_shot = function(player, itemstack, index)
 			if index >= player:get_inventory():get_size("main") and not def.throw_itself then
 				return false
 			end
-			return throwing.is_arrow(itemstack)
+			return throwing.is_arrow(itemstack) or def.throw_itself
 		end
 	end
+
 	if not def.inventory_image then
 		def.inventory_image = def.texture
 	end
+
+	if not def.strength then
+		def.strength = 20
+	end
+
 	def.on_use = function(itemstack, user, pointed_thing)
 		-- Cooldown
 		local meta = itemstack:get_meta()
@@ -357,9 +410,18 @@ function throwing.register_bow(name, def)
 			end
 
 			-- Shoot arrow
-			if shoot_arrow(itemstack, user, bow_index, def.throw_itself, new_stack) then
+			local uses = 65535 / (def.uses or 50)
+			local toolranks_data
+			if enable_toolranks then
+				toolranks_data = {
+					itemstack = itemstack,
+					index = bow_index,
+					wear = uses
+				}
+			end
+			if shoot_arrow(def, toolranks_data, user, bow_index, def.throw_itself, new_stack) then
 				if not minetest.settings:get_bool("creative_mode") then
-					itemstack:add_wear(65535 / (def.uses or 50))
+					itemstack:add_wear(uses)
 				end
 			end
 
@@ -377,22 +439,11 @@ function throwing.register_bow(name, def)
 		end)
 		return itemstack
 	end
-	minetest.register_tool(name, def)
 
-	if def.itemcraft then
-		-- Check for an override name
-		-- because an ``output = ":name"'' can't exist in a recipe
-		local output_name = name
-		if name:sub(1,1) == ":" then
-			output_name = name:sub(2)
-		end
-		minetest.register_craft({
-			output = output_name,
-			recipe = {
-				{"farming:cotton", def.itemcraft, ""},
-				{"farming:cotton", "", def.itemcraft},
-				{"farming:cotton", def.itemcraft, ""},
-			}
-		})
+	if enable_toolranks then
+		def.original_description = def.original_description or def.description
+		def.description = toolranks.create_description(def.description, 0, 1)
 	end
+
+	minetest.register_tool(name, def)
 end
