234e70e42fcdf65798930fd2b7ff0bb23c4b0fa2
upsilon <upsilon@langg.net>
2020-05-23 22:07:39 +0200

    git subrepo clone https://github.com/joe7575/techpack.git mods/techpack
    subrepo:
      subdir:   "mods/techpack"
      merged:   "82fcfad14"
    upstream:
      origin:   "https://github.com/joe7575/techpack.git"
      branch:   "master"
      commit:   "82fcfad14"
    git-subrepo:
      version:  "0.4.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a04d8c2"

---
+++ mods/techpack/tubelib_addons3/init.lua
@@ -0,0 +1,18 @@
+--[[
+
+	Tubelib Addons 3
+	================
+
+	Copyright (C) 2018 Joachim Stolberg
+
+	LGPLv2.1+
+	See LICENSE.txt for more information
+
+]]--
+
+dofile(minetest.get_modpath("tubelib_addons3") .. '/chest.lua')
+dofile(minetest.get_modpath("tubelib_addons3") .. '/pusher.lua')
+dofile(minetest.get_modpath("tubelib_addons3") .. '/distributor.lua')
+dofile(minetest.get_modpath("tubelib_addons3") .. '/pushing_chest.lua')
+dofile(minetest.get_modpath("tubelib_addons3") .. '/teleporter.lua')
+dofile(minetest.get_modpath("tubelib_addons3") .. '/funnel.lua')
