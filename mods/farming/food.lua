03f1197ba90ab576b27221d42228d6a5f72d39da
upsilon <upsilon@langg.net>
2021-02-08 11:40:27 +0100

    git subrepo pull mods/farming
    subrepo:
      subdir:   "mods/farming"
      merged:   "79035d047"
    upstream:
      origin:   "https://notabug.org/TenPlus1/farming.git"
      branch:   "master"
      commit:   "79035d047"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "2f68596"

--- mods/farming/food.lua
+++ mods/farming/food.lua
@@ -181,44 +181,18 @@ minetest.register_craftitem("farming:porridge", {
 	on_use = minetest.item_eat(6, "farming:bowl")
 })
 
-minetest.after(0, function()
-
-	local fluid = "group:water_bucket"
-	local fluid_return = {
-		{"group:water_bucket", "bucket:bucket_empty"},
-		{"group:water_bucket_wooden", "bucket:bucket_empty"}
+minetest.register_craft({
+	type = "shapeless",
+	output = "farming:porridge",
+	recipe = {
+		"group:food_oats", "group:food_oats", "group:food_oats",
+		"group:food_oats", "group:food_bowl", "group:food_milk_glass"
+	},
+	replacements = {
+		{"mobs:glass_milk", "vessels:drinking_glass"},
+		{"farming:soy_milk", "vessels:drinking_glass"}
 	}
-
-	if minetest.get_modpath("mobs") and mobs and mobs.mod == "redo" then
-		fluid = "group:food_milk"
-		fluid_return = {
-			{"mobs:bucket_milk", "bucket:bucket_empty"}
-		}
-	else
-		minetest.register_craft({
-			type = "shapeless",
-			output = "farming:porridge",
-			recipe = {
-				"group:food_oats", "group:food_oats", "group:food_oats",
-				"group:food_oats", "group:food_bowl", "group:water_bucket_wooden"
-			},
-			replacements = fluid_return
-		})
-	end
-
-	minetest.register_craft({
-		type = "shapeless",
-		output = "farming:porridge",
-		recipe = {
-			"group:food_oats", "group:food_oats", "group:food_oats",
-			"group:food_oats", "group:food_bowl", fluid
-		},
-		replacements = fluid_return
-	})
-
-	if minetest.get_modpath("bucket_wooden") then
-	end
-end)
+})
 
 --= Jaffa Cake
 
@@ -373,3 +347,57 @@ minetest.register_craft({
 	replacements = {{"group:food_skillet", "farming:skillet"}}
 })
 end
+
+-- Burger
+
+minetest.register_craftitem("farming:burger", {
+	description = S("Burger"),
+	inventory_image = "farming_burger.png",
+	on_use = minetest.item_eat(16),
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "farming:burger",
+	recipe = {
+		"farming:bread", "group:food_meat", "group:food_cheese",
+		"group:food_tomato", "group:food_cucumber", "group:food_onion",
+		"group:food_lettuce"
+	}
+})
+
+-- Salad
+
+minetest.register_craftitem("farming:salad", {
+	description = S("Salad"),
+	inventory_image = "farming_salad.png",
+	on_use = minetest.item_eat(8, "farming:bowl")
+})
+
+minetest.register_craft({
+	output = "farming:salad",
+	type = "shapeless",
+	recipe = {
+		"group:food_bowl", "group:food_tomato", "group:food_cucumber",
+		"group:food_lettuce", "group:food_oil"
+	},
+})
+
+-- Triple Berry Smoothie
+
+minetest.register_craftitem("farming:smoothie_berry", {
+	description = S("Triple Berry Smoothie"),
+	inventory_image = "farming_berry_smoothie.png",
+	on_use = minetest.item_eat(6, "vessels:drinking_glass"),
+	groups = {vessel = 1, drink = 1}
+})
+
+minetest.register_craft({
+	output = "farming:smoothie_berry",
+	type = "shapeless",
+	recipe = {
+		"group:food_raspberries", "group:food_blackberries",
+		"group:food_strawberry", "group:food_banana",
+		"vessels:drinking_glass"
+	}
+})
